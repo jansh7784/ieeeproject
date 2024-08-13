4b9e3769696f8847f795c8cdaf602d782eff6f5d
Ansh Jain <142454870+jansh7784@users.noreply.github.com>
2024-08-14 00:18:37 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import './Footer.css';
 
+const Footer = () => {
+  return (
+    <footer>
+      <p>Follow us on:
+        <a href="#">Twitter</a> | 
+        <a href="#">Facebook</a> | 
+        <a href="#">Instagram</a>
+      </p>
+      <p>Contact us at <a href="mailto:email@example.com">email@example.com</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
