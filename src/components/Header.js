97c6de7ffef7d160b4c70f34cc41efdf1be82e16
Ansh Jain <142454870+jansh7784@users.noreply.github.com>
2024-08-14 00:15:30 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><NavLink to="/" exact activeClassName="active">Home</NavLink></li>
+          <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+          <li><NavLink to="/events" activeClassName="active">Events</NavLink></li>
+          <li><NavLink to="/team" activeClassName="active">Team</NavLink></li>
+          <li><NavLink to="/contact" activeClassName="active">Contact</NavLink></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
